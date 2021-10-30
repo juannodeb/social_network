62acd89881aeaabf535f38788752457810b6c135
juannodeb <juannodeb@gmail.com>
2021-10-30 16:42:21 -0500

    feat: Added @popperjs/core package and updated some js files for javascript to work

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,12 @@
-import { environment } from '@rails/webpacker'
+const { environment } = require('@rails/webpacker')
 
-import { ProvidePlugin } from 'webpack'
-environment.plugins.append(
-  'Provide',
-  new ProvidePlugin({
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default'] 
   })
 )
 
-export default environment
+module.exports = environment
